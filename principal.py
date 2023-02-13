9bc57098ca3ed387528954aaf732e70de056095c
Claverias123 <al_amartinezc@iessierradelasvillas.org>
2023-02-13 10:58:06 +0100

    paginaprincipal

--- principal.py
+++ principal.py
@@ -1,7 +1,21 @@
 import flet as ft
 def main(page: ft.Page):
-    page.title = "Images Example"
-    page.bgcolor=ft.colors.PURPLE_100
-    page.padding = 50
+    page.title = "INICIO DE SESIÓN"
+    page.bgcolor=ft.colors.PURPLE_300
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    page.window_height=500
+    page.window_width=500
     page.update()
+
+
+    tfnombre=ft.TextField(label="Nombre")
+    tfpassword=ft.TextField(label="Contraseña")
+    colDatos=ft.Column(controls=[tfnombre,tfpassword])
+    contDatos=ft.Container(content=colDatos, bgcolor=ft.colors.PURPLE_100,width=100,padding=ft.padding.only(bottom=50))
+
+
+    txt=ft.Text("Hola")
+    page.add(txt,contDatos)
+
+
 ft.app(target=main)
