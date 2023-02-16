493ccaa673ea119d3c0f6e3d0d9ba8be9cac8b6e
Claverias123 <al_amartinezc@iessierradelasvillas.org>
2023-02-16 10:15:29 +0100

    poco a poco

--- principal.py
+++ principal.py
@@ -5,24 +5,38 @@ def main(page: ft.Page):
     page.title = "INICIO DE SESIÓN"
     page.bgcolor=ft.colors.PURPLE_300
     page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
-    page.window_height=500
+    page.window_height=850
     page.window_width=500
     page.update()
     #------------- fin del diseño de página ---------------
 
-    #------------- Función del botón ---------------------
-    def botónGuardarNombreY ():
-        vNombre=[]
-        f=open("Equipos.txt", "r")
+    #--------- función notificación usu y contr correcta-----------
+    def open_dlg(e):
+        page.dialog = dlg
+        dlg.open = True
+        page.update()
 
-        linea = f.readline()
-        vNombre = linea.split(sep=";")
+    #------------- Función del botón ----------------------
+    def botónGuardarNombreY (w):
+        vVariable=[]
+        i=open("archivo.txt", "r")
+
+        for linea in i:
+            t = linea.replace("\n","")
+            vVariable.append(t)
+        print (vVariable)
+
+        if colDatos==vVariable:
+            vVariable=[]
+        print(open_dlg)
+
+
             
 
-        f.close()
-        return vNombre
+        
     page.update()                           
   
+
 
 
     #----------------- variables----------------------
@@ -30,12 +44,14 @@ def main(page: ft.Page):
                     width=500,
                     height=500)
 
-    boton=ft.FilledButton("Iniciar sesión")
+    boton=ft.FilledButton("Iniciar sesión", on_click=botónGuardarNombreY)
     tfnombre=ft.TextField(label="Nombre")
     tfpassword=ft.TextField(label="Contraseña", password= True ,can_reveal_password= True, width=500)
     colDatos=ft.Column(controls=[tfnombre,tfpassword])
     contDatos=ft.Container(content=colDatos, bgcolor=ft.colors.PURPLE_300,width=200,padding=ft.padding.only(bottom=100)) 
+    dlg=ft.AlertDialog(title=ft.Text("Inicio de sesión correcto"))
  
+
     
     
     
